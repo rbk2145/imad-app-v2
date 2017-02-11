15f5ef994025d6e5bdbace1ed4ae13e9e765a501
rbk2145 <rbk2145@gmail.com>
2017-02-11 19:11:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 app.get('/about-us',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'about-us.html'));
 });
-app.get('/contacts',function(req,res){
+app.get('/contact',function(req,res){
     res.send('Article two required and will be severd here');
 });
 app.get('/downloads',function(req,res){
