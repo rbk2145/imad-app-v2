fb7916845bbc445c545a1e6ffc3f2d7bd0bd0684
rbk2145 <rbk2145@gmail.com>
2017-02-12 10:52:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML="Welcome to tech krishna world, A Complete CS world";
 // move image 
 var img =document.getElementById('move-img');
-img.onclick=function{
+img.onclick=function(){
     img.style.marginleft='100px';
     
 };
