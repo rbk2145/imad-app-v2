c81de34f58e3ac795da146dc6d02ccb1a21cbbd9
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2017-02-14 22:57:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/downloads',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'downloads.html'));
 });
 app.get('/News-Feed',function(req,res){
-    res.send('Article three required and will be severd here');
+    res.sendFile(path.join(__dirname, 'ui', 'News-Feed.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
