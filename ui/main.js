fff4ab55959ffcb0805d13d6afebf54697d93b06
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2017-02-14 23:23:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML="Welcome to tech krishna world, A Complete CS world";
 var img =document.getElementById('move-img');
 var marginLeft=0;
 function  moveright(){
-    marginLeft=marginLeft+5;
+    marginLeft=marginLeft+1;
     img.syle.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
