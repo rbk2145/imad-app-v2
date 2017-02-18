6c1ad8219669b6855afcef8961d9afa2fa99ea0f
Radha Krishna Murthy Pinnamaneni <rbk2145@gmail.com>
2017-02-19 00:57:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,60 @@
 console.log('Loaded!');
+// counter code
+var button = document.getElementById("counter");
 
-// changing txt of main page
-//var element = document.getElementById('main-text');
-//element.innerHTML="Welcome to tech krishna world, A Complete CS world";
-// move image 
-var img =document.getElementById('move-img');
-var marginLeft=0;
-function  moveright(){
-    marginLeft=marginLeft+1;
-    img.syle.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+button.onclick = function() {
     
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById("count");
+              span.innerHTML = counter.toString();
+          }
+      }  
+      // not done yet
+    };
+    
+    // make the request
+    request.open('GET', 'http://rbk2145.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
+
+
+// submit name
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function() {
+    
+    // create a request object
+    var request = new XMLHttpRequest();
+    
+    // capture the response and store it in a variable
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // take some action
+          if (request.status === 200) {
+              // capture a list of names and render it
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = "";
+              for (var i=0; i<names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+          }
+      }  
+      // not done yet
+    };
+    
+    // make the request
+    var nameInput = document.getElementById("name");
+    var name = nameInput.value;
+    request.open('GET', 'http://darkfist.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
